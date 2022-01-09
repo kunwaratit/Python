a8028402280910714704be78587f69318f5725bf
Atit Kunwar <atitkunwar2015@gmail.com>
2022-01-09 09:46:18 +0545

    update

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -1,14 +1,26 @@
 
-while True:
+maxGuess = 5
+guessCount = 0
+chance = maxGuess
+while guessCount < maxGuess:
     import random
-    comp_num=random.randint(1,10)
-    user_num=int(input('enter your guess:'))
-    print('Your guess:',user_num)
-    print('Computer Guess:',comp_num)
-    if user_num<=10:    
-        if comp_num==user_num:
-            print('You won')
-        else :
-            print('you lose')
+    comp_num = random.randint(1, 10)
+
+    guessCount = guessCount+1
+    print(f'You have {chance} chances to guess:')
+    user_num = int(input(f'{guessCount}. enter your guess:'))
+    print(f'Your guess:', user_num)
+    print('Computer Guess:', comp_num)
+
+    if user_num <= 10:
+        chance -= 1
+        if comp_num == user_num:
+            print('Congratulation! You Won The Game')
+            break
+        elif guessCount == maxGuess:
+            print('Not Matched, You Lose The Game!')
+        else:
+            print('Not Matched')
     else:
+        guessCount = guessCount-1
         print('Out of Range')
