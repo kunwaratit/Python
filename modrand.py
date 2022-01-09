47d0820a566120b9255fc25db402eff5fa994a27
Atit Kunwar <atitkunwar2015@gmail.com>
2022-01-09 08:43:43 +0545

    nn

---
+++ modrand.py
@@ -0,0 +1,13 @@
+import random
+
+
+def range():
+    num1 = int(input('enter 1strange:'))
+    num2 = int(input('enter lastrange:'))
+    num = [1, 2, 3, 4, 5, 6, 7]
+    print(num[num1-1:num2])
+
+
+ss = range()
+
+22 + 5
